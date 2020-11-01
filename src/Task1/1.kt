f21b9c6c6cf6a1a9e38b424a8db7fdec7a684f76
ACER <galihtririskyandiko@gmail.com>
2020-11-01 05:00:50 -0800

    update

--- src/Task1/1.kt
+++ src/Task1/1.kt
@@ -1,10 +1,11 @@
 package Task1
 
-fun sayHello(firstname: String, lastname: String): String {
-    return "nama saya adalah $firstname $lastname"
+fun add(value1: Double, value2: Double): String {
+    val total =  value1 + value2
+    return "hasil dari $value1 + $value2 = $total "
 }
 
 fun main() {
-    val name = sayHello("galih","tri risky andiko")
-    println(name)
+    val math = add(10.0,40.0)
+    println(math)
 }
